e29cbb3d406b57bfefdb78e87a7718cd86c0a675
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-03-13 14:53:50 +0800

    Add Direction enum and use it in code

---
+++ enums.ts
@@ -0,0 +1,11 @@
+enum Direction {
+    Up = 'UP',
+    Down = 'DOWN',
+    Left = 'LEFT',
+    Right = 'RIGHT',
+}
+
+const value = 'UP';
+if (value === Direction.Up) {
+    console.log('GO UP!');
+}
