5d8bc3977b134d15c13f035b32263e4ff0814601
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-03-13 15:44:29 +0800

    Add generic constraints for echoWithLength function

--- generics.ts
+++ generics.ts
@@ -8,3 +8,23 @@ function swap<T, U>(tuple: [T, U]): [U, T] {
   return [tuple[1], tuple[0]];
 }
 const result = swap([7, "seven"]); // result: [string, number]
+
+// Generic Constraints
+// 只允许传入包含 .length 属性的变量
+function echoWithArr<T>(arg: T[]): T[] {
+  console.log(arg.length);
+  return arg;
+}
+
+interface IWithLength {
+  length: number;
+}
+function echoWithLength<T extends IWithLength>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+
+const str2 = echoWithLength("str");
+const obj = echoWithLength({ length: 10, width: 10 });
+const arr2 = echoWithLength([1, 2, 3]);
+echoWithLength(7); // Error
